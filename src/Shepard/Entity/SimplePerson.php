72b4e1e883d7de8e72482d389657bb2758753e5f
juhaszadam <juhasz.adam@virgo.hu>
2015-06-24 09:20:38 +0200

    SimplePerson C2

--- src/Shepard/Entity/SimplePerson.php
+++ src/Shepard/Entity/SimplePerson.php
@@ -2,7 +2,70 @@
 
 namespace Shepard\Entity;
 
-class SimplePerson 
+class SimplePerson
 {
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     */
+    private $bornIn;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getBornIn()
+    {
+        return $this->bornIn;
+    }
+
+    /**
+     * @param \DateTime $bornIn
+     */
+    public function setBornIn($bornIn)
+    {
+        $this->bornIn = $bornIn;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
 
 }
